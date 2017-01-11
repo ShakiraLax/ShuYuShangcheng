3b53672fc0a5b3563fc241d70a2677cf6af74a00
lax <1158152686@qq.com>
2017-01-11 09:58:02 +0800

    搜索界面

---
+++ app/src/main/java/com/sypm/shuyushangcheng/moudle/classify/SearchResultActivity.java
@@ -0,0 +1,15 @@
+package com.sypm.shuyushangcheng.moudle.classify;
+
+import android.os.Bundle;
+
+import com.sypm.shuyushangcheng.R;
+import com.sypm.shuyushangcheng.base.BaseActivity;
+
+public class SearchResultActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search_result);
+    }
+}
