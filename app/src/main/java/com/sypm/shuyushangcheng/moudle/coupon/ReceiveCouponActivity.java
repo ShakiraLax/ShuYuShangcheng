ae0e3def1679ae67631e8d6baaa9c8c79f0ac026
lax <1158152686@qq.com>
2017-01-12 17:54:36 +0800

    领券中心

---
+++ app/src/main/java/com/sypm/shuyushangcheng/moudle/coupon/ReceiveCouponActivity.java
@@ -0,0 +1,15 @@
+package com.sypm.shuyushangcheng.moudle.coupon;
+
+import android.os.Bundle;
+
+import com.sypm.shuyushangcheng.R;
+import com.sypm.shuyushangcheng.base.BaseActivity;
+
+public class ReceiveCouponActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_receive_coupon);
+    }
+}
