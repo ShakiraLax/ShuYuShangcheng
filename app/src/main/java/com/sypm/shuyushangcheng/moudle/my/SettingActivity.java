b9c14c0a8a14a1460c946a3fcc99d37378431c97
lax <1158152686@qq.com>
2017-01-12 11:22:16 +0800

    设置界面

---
+++ app/src/main/java/com/sypm/shuyushangcheng/moudle/my/SettingActivity.java
@@ -0,0 +1,15 @@
+package com.sypm.shuyushangcheng.moudle.my;
+
+import android.os.Bundle;
+
+import com.sypm.shuyushangcheng.R;
+import com.sypm.shuyushangcheng.base.BaseActivity;
+
+public class SettingActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_setting);
+    }
+}
