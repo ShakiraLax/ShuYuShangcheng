e68caa37cb68175a0c5102ec315e44fdb00bd444
lax <1158152686@qq.com>
2017-02-17 17:57:57 +0800

    订单结算

---
+++ app/src/main/java/com/sypm/shuyushangcheng/moudle/shoppingcart/OrderSettlementActivity.java
@@ -0,0 +1,15 @@
+package com.sypm.shuyushangcheng.moudle.shoppingcart;
+
+import android.os.Bundle;
+
+import com.sypm.shuyushangcheng.R;
+import com.sypm.shuyushangcheng.base.BaseActivity;
+
+public class OrderSettlementActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_order_settlement);
+    }
+}
